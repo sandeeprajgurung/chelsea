c54a90a40bbcd46b6fc31ba2b8aa6b0900c7d7ef
sandeeprajgurung <smile2srg@gmail.com>
2021-07-01 22:38:37 +0545

    admin setup

---
+++ view/admin/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include_once './partials/header.php'; ?>
+</head>
+
+<body>
+    <?php include_once './partials/navbar.php'; ?>
+
+    <?php include_once './partials/script.php'; ?>
+</body>
+
+</html>
---
+++ view/admin/partials/script.php
@@ -0,0 +1,1 @@
+<script type="text/javascript" src="../../resources/assets/mdb/js/mdb.min.js"></script>
